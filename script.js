7211ff4e4a1e5f620d3df4dc544666e9b009e076
miyevu <keklemyevu@gmail.com>
2024-03-22 01:38:22 +0000

    progress

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menuIcon = document.querySelector('.checkbtn');
+    var menuLines = document.querySelectorAll('.hamburger-lines');
+    menuIcon.classList.toggle('cross');
+    menuLines[0].classList.toggle('cross-line-first');
+    menuLines[2].classList.toggle('cross-line-last');
+    menuLines[1].classList.toggle('hide-line');
+}
